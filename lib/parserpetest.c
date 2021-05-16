44e33f90962fcf62c7c44ba4037ff26473628209
ges-cs01 <ges.cs01@gmail.com>
2021-05-16 04:53:37 -0300

    add more headers

--- lib/parserpetest.c
+++ lib/parserpetest.c
@@ -1,5 +1,27 @@
 #include "parserpetest.h" 
 
-bool parserpetest_ispe(const unsigned char *buffer) {
-    return (buffer[0] == 'M' && buffer[1] == 'Z');
+bool parserpetest_ispe(PEFILE *pe) {
+    return (pe->hdr_dos->e_magic == 0x5a4d); //'MZ'
+}
+
+bool parserpetest_init(PEFILE *pe) {
+    FILE *file = fopen(pe->filepath, "rb");
+    
+    if(!file) 
+        return false;
+    
+    pe->hdr_dos = malloc(sizeof(IMAGE_DOS_HEADER));
+    if(!pe->hdr_dos)
+        return false;
+
+    fread(pe->hdr_dos, sizeof(IMAGE_DOS_HEADER), 1, file);
+    
+    fclose(file);
+    
+    return true;
+}
+
+void parserpetest_deinit(PEFILE *pe) {
+    if(pe->hdr_dos) 
+        free(pe->hdr_dos);
 }
